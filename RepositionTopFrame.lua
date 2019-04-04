aca01263c523b913bb1faf6fe0fc9c1375e3493c
Arne Kvaleberg <arnekva@gmail.com>
2019-04-04 21:06:47 +0200

    Create RepositionTopFrame.lua

---
+++ RepositionTopFrame.lua
@@ -0,0 +1,10 @@
+local frame = CreateFrame("Frame")
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_LOGIN")
+
+f:SetScript("OnEvent", function()
+	UIWidgetTopCenterContainerFrame:ClearAllPoints()  UIWidgetTopCenterContainerFrame:SetPoint("TOP", 0, -50)
+
+	print("\124cffffff00\124h<RepositionTopFrame>\124cFF00FF00\124h Top Center Frame is now repositioned!")
+end)
